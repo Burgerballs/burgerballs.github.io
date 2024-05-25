2c055774e48c963a0dc7f14ddc07998a88761028
Burgerballs <107233412+Burgerballs@users.noreply.github.com>
2024-05-25 17:27:04 +0100

    fsdfonwdfojiwef

--- gamebananamods.js
+++ gamebananamods.js
@@ -15,7 +15,7 @@ function makeText(filename) {
     var re = /,/;
     var headers = swag.shift().split(re);
     var res_names = makeResArray(swag, headers, 'mod_name');
-    var res_year = makeResArray(swag, headers, 'work_time');
+    var res_year = makeResArray(swag, headers, 'release');
     var res_image = makeResArray(swag, headers, 'mod_image');
     var res_id = makeResArray(swag, headers, 'mod_id');
     var text = "";
@@ -26,7 +26,7 @@ function makeText(filename) {
         text += ' <small>'+res_year[i]+'</small><br>';
         text += '<img src="'+res_image[i]+'" height=480>';
         if (res_id[i] != 'none') {
-            text += '<a href="https://gamebanana.com/mods/'+res_id[i]+'"><div id="fuck">'+'<img src="gothere.png" height=128>'+'</div></a>'
+            text += '<a href="'+res_id[i]+'"><div id="fuck">'+'<img src="gothere.png" height=128>'+'</div></a>'
         }
         
         text += '</div>';
